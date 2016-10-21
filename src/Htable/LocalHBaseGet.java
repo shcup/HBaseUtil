dde1d425e0b141d4cf3cbfd690ad75851fef6449
zhanglin5 <zhanglin5@CP804Z1.letv.local>
2016-10-21 16:39:18 +0800

    LocalHBaseGet from files

--- src/Htable/LocalHBaseGet.java
+++ src/Htable/LocalHBaseGet.java
@@ -1,10 +1,18 @@
 package Htable;
 
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.Get;
@@ -12,48 +20,40 @@ import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+
+import DocProcess.CompositeDocSerialize;
+import pipeline.CompositeDoc;
 
 public class LocalHBaseGet {      
 	
-	public static void main(String[] args) throws IOException {
-        Configuration conf = new Configuration();  
-        //与hbase/conf/hbase-site.xml中hbase.zookeeper.quorum配置的值相同   
-        conf.set("conf.column", "info"); 
-        //与hbase/conf/hbase-site.xml中hbase.zookeeper.property.clientPort配置的值相同 
-    	conf.set("hbase.zookeeper.property.clientPort", "31818");
-        conf.set("hbase.rootdir", "hdfs://in-cluster/hbase");   
-        conf.set("hbase.zookeeper.quorum", "in-cluster-namenode1,in-cluster-namenode2,in-cluster-logserver");
-        conf = HBaseConfiguration.create(conf); 
-
-//      HTable table=new HTable(conf,"Localltable");        
-        HTable table=new HTable(conf,args[0]);
-        
-		Get get = new Get(Bytes.toBytes(args[1]));  
- 		if(get==null){
-			System.out.println("The media_doc_id does not exist");
-		}else{
-		Result rs = table.get(get);  
-		FileWriter writer=new FileWriter(args[2],true);	
+	public static void main(String[] args) throws IOException{	
 		
-		byte[] value=rs.getValue(Bytes.toBytes("info"),Bytes.toBytes("context"));
-		byte[] value1=rs.getValue(Bytes.toBytes("info"),Bytes.toBytes("media_doc_id"));
-//	     for(KeyValue kv : rs.raw()){  
-//	    	 String s=new String(kv.getValue())+"\t";
-		String s=Bytes.toString(value);
-		String id=Bytes.toString(value1);
-	 		writer.write(id+"\t"+s);
-  
-//	            System.out.print(new String(kv.getRow()) + " " );  
-//	            System.out.print(new String(kv.getFamily()) + ":" );  
-//	            System.out.print(new String(kv.getQualifier()) + " " );  
-//	            System.out.print(kv.getTimestamp() + " " );  
-//	            System.out.println(new String(kv.getValue()));  
-//	        }
-	     writer.write("\n");
-	 		if(writer != null){  
-                writer.close();     
-            }
+        Configuration conf = new Configuration();            
+        conf.set("conf.column", "info"); 
+        
+    	conf.set("hbase.zookeeper.property.clientPort", "31818"); //与hbase/conf/hbase-site.xml中hbase.zookeeper.property.clientPort配置的值相同 
+        conf.set("hbase.rootdir", "hdfs://in-cluster/hbase");   
+        conf.set("hbase.zookeeper.quorum", "in-cluster-namenode1,in-cluster-namenode2,in-cluster-logserver");//与hbase/conf/hbase-site.xml中hbase.zookeeper.quorum配置的值相同
+        conf = HBaseConfiguration.create(conf);         
+        HTable table = new HTable(conf,args[0]); 
+        
+        File f = new File(args[1]); //Read file 
+        FileWriter writer = new FileWriter(args[2],true);//write result
+    	BufferedReader br1=new BufferedReader (new InputStreamReader(new FileInputStream(f)));
+    	
+        String ID = null;
+    	while ((ID = br1.readLine()) != null){
+    		Get get = new Get(Bytes.toBytes(ID));  
+    		Result rs = table.get(get); 	
+    		byte[] value=rs.getValue(Bytes.toBytes("info"),Bytes.toBytes("context"));
+    		byte[] value1=rs.getValue(Bytes.toBytes("info"),Bytes.toBytes("media_doc_id"));
+    		String text=Bytes.toString(value1)+"\t"+Bytes.toString(value);
+    		writer.write(text+"\n"); 
+    		}
+    	
+    	if(writer != null){
+    		writer.close();
+    		}
 	}
-	}
-
 }
