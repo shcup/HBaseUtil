b3178db2460d308cf6068745c29030fd0063ed18
sunhaochuan <sunhaochuan@DESKTOP-UAIMORT.letv.local>
2016-10-21 22:07:55 +0800

    add debug string

--- src/Htable/LocalHBaseGetConsole.java
+++ src/Htable/LocalHBaseGetConsole.java
@@ -30,9 +30,16 @@ public class LocalHBaseGetConsole {
         System.out.println("«Î ‰»ÎID:");
     	Scanner scan=new Scanner(System.in);
 		while(scan.hasNext()){
-			String s=scan.nextLine();			
+			String s=scan.nextLine();	
+			if (s.isEmpty()) {
+				continue;
+			}
     		Get get = new Get(Bytes.toBytes(s));  
     		Result rs = table.get(get); 	
+    		if (rs == null || rs.size() == 0) {
+    			System.out.println("Empty Key!");
+    			continue;
+    		}
     		byte[] value=rs.getValue(Bytes.toBytes("info"),Bytes.toBytes("context"));
     		byte[] value1=rs.getValue(Bytes.toBytes("info"),Bytes.toBytes("media_doc_id"));
     		String text=Bytes.toString(value);	
@@ -40,12 +47,49 @@ public class LocalHBaseGetConsole {
     		List<String> bodylist=new ArrayList<String>();
     		Context context=null;
     		CompositeDoc compositeDoc = CompositeDocSerialize.DeSerialize(text, context);
-    		title=compositeDoc.title;
-    		bodylist=compositeDoc.body_words;  		
-    		
-    		System.out.println("title:\t"+title);
-    		System.out.println("body:\t"+bodylist.toString());
+    		Print(compositeDoc);
 		}
 
 	}	
+	
+	
+	public static void Print(CompositeDoc compositeDoc) {
+		System.out.println("title:\t" + compositeDoc.title);
+		System.out.println("URL:\t" + compositeDoc.doc_url);
+		System.out.println("Domain:\t" + compositeDoc.media_doc_info.source);
+		System.out.println("Language type:\t" + compositeDoc.language_type.toString());
+		System.out.println("Show Time:\t" + compositeDoc.showtime);
+		System.out.println("Content timestamp\t" + compositeDoc.media_doc_info.content_timestamp);
+		System.out.println("Update timestamp\t" + compositeDoc.media_doc_info.update_timestamp);
+		System.out.println("Crawler timestamp\t" + compositeDoc.media_doc_info.crawler_timestamp);
+		System.out.println("Is headline\t" + compositeDoc.media_doc_info.is_headline);
+		System.out.println("Is home\t" + compositeDoc.media_doc_info.is_home);
+		System.out.println("Source name:\t" + compositeDoc.source_name);
+		System.out.println("Source weight:\t" + compositeDoc.source_weight);
+		System.out.println("Title NER\t" + compositeDoc.title_ner.toString());
+		System.out.println("Body Ner\t" + compositeDoc.body_ner.toString());
+		System.out.println("Title NP\t" + compositeDoc.title_np.toString());
+		System.out.println("Body NP\t" + compositeDoc.body_np.toString());
+		System.out.println("Title NNP\t" + compositeDoc.title_nnp.toString());
+		System.out.println("Body NNP\t" + compositeDoc.body_nnp.toString());
+		System.out.println("Text rank\t" + compositeDoc.text_rank.toString());
+		System.out.println("Text rank phrase\t" + compositeDoc.text_rank_phrase.toString());
+		String out;
+		if (compositeDoc.media_doc_info.normalized_category_info == null) {
+			out = "NULL";
+		} else {
+			out = compositeDoc.media_doc_info.normalized_category_info.toString();
+		}
+		System.out.println("Normalized category:\t" + out);
+		if (compositeDoc.media_doc_info.classified_category_info == null) {
+			out = "NULL";
+		} else {
+			out = compositeDoc.media_doc_info.classified_category_info.toString();
+		}
+		System.out.println("Classified category:\t" + out);
+		//System.out.println();
+		
+		
+		//System.out.println("body:\t"+compositeDoc.main_text_list.toString());
+	}
 }
